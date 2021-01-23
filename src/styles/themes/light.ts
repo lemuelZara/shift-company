c3c3a33ec7a9fc32f273762c41fa9d05d3b5568e
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-23 16:28:21 -0300

    feat: add light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,41 @@
+export default {
+  colors: {
+    primary: '#023e8a',
+    primaryDark: '#03045e',
+    secondary: '#0096c7',
+    gray: '#ccc',
+    grayLowerOpacity: '#9C9C9C',
+    lightGray: '#636363',
+    darkGray: '#383838',
+    success: '#20c05c',
+    error: '#fd4f48',
+    label: '#ffffff',
+    background: '#ffffff',
+  },
+  fontSizes: {
+    default: '1.4rem',
+    large: '1.6rem',
+    small: '1.2rem',
+    tiny: '1rem',
+  },
+  spacing: {
+    default: '1.6rem',
+    vertical: '1.6rem 0',
+    horizontal: '0 1.6rem',
+    large: '2.4rem',
+  },
+  transition: {
+    default: '180ms ease-in-out',
+  },
+  radii: {
+    default: '0.8rem',
+    small: '0.4rem',
+    smallTop: '0.4rem 0.4rem 0 0',
+    smallBottom: '0 0 0.4rem 0.4rem',
+    tiny: '0.2rem',
+  },
+  shadows: {
+    default: '0 0.5rem 2rem rgba(0, 0, 0, 0.08)',
+    flat: '0 0.2rem 0.2rem rgba(0, 0, 0, 0.8)',
+  },
+};
