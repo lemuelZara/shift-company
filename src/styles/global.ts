e0f4b128f1bb7e3e440c1b3121a5b239ccca9942
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-23 14:51:11 -0300

    feat: add global styles for app

---
+++ src/styles/global.ts
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+    height: 100%;
+  }
+
+  body {
+    height: auto;
+    background: #f9faff;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, #root {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    position: relative;
+  }
+
+  #root {
+    height: 100%;
+  }
+  
+  body, input, button {
+    font: 1.6rem 'Lato', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto;
+    line-height: 1.48;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  a {
+    text-decoration: none;
+    background: none;
+    font-weight: 700;
+    cursor: pointer;
+    border: 0;
+    transition: 180ms ease-in-out;
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+    border: 0;    
+  }
+
+  ul {
+    list-style: none;
+    text-align: left;
+    padding: 0;
+  }
+`;
