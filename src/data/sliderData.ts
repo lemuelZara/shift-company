d6b520147f6f3834ee0b546d89b060ddfa9ab70e
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-24 02:03:47 -0300

    refactor: move slides info to a data object

---
+++ src/data/sliderData.ts
@@ -0,0 +1,25 @@
+import ImageOne from '../assets/images/image_slider_one.jpg';
+import ImageTwo from '../assets/images/image_slider_two.jpg';
+
+export const sliderData = [
+  {
+    id: 1,
+    title: 'Soluções completas em software para laboratórios clínicos',
+    subTitle:
+      'Fruto de mais de 25 anos de experiência e pioneirismo em tecnologia da informação para medicina diagnóstica preventiva',
+    label: 'Conheça nossas soluções',
+    image: ImageOne,
+    imageSrcSet: ImageOne,
+    alt: 'uma mesa com microscópios',
+  },
+  {
+    id: 2,
+    title: 'Produtividade e segurança na Gestão Laboratorial',
+    subTitle:
+      'Gerencie de modo eficaz todos os processos e setores do laboratório',
+    label: 'Conheça a Shift LIS',
+    image: ImageTwo,
+    imageSrcSet: ImageTwo,
+    alt: 'uma mesa com pessoas e notebooks',
+  },
+];
