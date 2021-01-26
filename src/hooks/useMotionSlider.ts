d0e44accbad605592485d498728db43ca8a530d2
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-26 00:40:22 -0300

    feat: create hook to motion slider

---
+++ src/hooks/useMotionSlider.ts
@@ -0,0 +1,24 @@
+import { useState, useCallback } from 'react';
+
+import { wrap } from 'popmotion';
+
+export interface UseMotionSliderData {
+  state: [number, number];
+  currentIndex: number;
+  handleChangeSlide: (newDirection: number) => void;
+}
+
+export const useMotionSlider = (data: any): UseMotionSliderData => {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const currentIndex = wrap(0, data.length, page);
+
+  const handleChangeSlide = (newDirection: number) => {
+    setPage(([prevPage]) => [prevPage + newDirection, newDirection]);
+  };
+
+  return {
+    state: [page, direction],
+    currentIndex,
+    handleChangeSlide,
+  };
+};
