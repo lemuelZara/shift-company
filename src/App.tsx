94c17af65e29a51c06c05c71ff4ae4e19c843fae
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-27 00:48:34 -0300

    refactor: import main content component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import Header from './components/Header';
 import * as themes from './styles/themes';
 import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import HeroImageSlider from './components/HeroImageSlider';
+import { sliderData } from './data/sliderData';
+import MainContent from './pages/MainContent';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={themes.light}>
       <Router>
         <Header />
+        <HeroImageSlider slides={sliderData} />
+
+        <MainContent />
 
         <GlobalStyles />
       </Router>
