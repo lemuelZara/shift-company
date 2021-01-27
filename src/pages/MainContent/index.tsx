a4375a655b90a55648d3620bbe9f871d51064004
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-27 00:48:09 -0300

    refactor: import services component

---
+++ src/pages/MainContent/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Services from './Services';
+
+const MainContent: React.FC = () => {
+  return (
+    <main>
+      <Services />
+    </main>
+  );
+};
+
+export default MainContent;
