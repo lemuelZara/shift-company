128914f747b9572fea06fa5fb441314d2cd90954
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-27 00:43:12 -0300

    refactor: remove container animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,15 +3,10 @@ import React, { memo } from 'react';
 import Navbar from './Navbar';
 
 import { AnimatedContainer } from './styles';
-import { CONTAINER_ANIMATION } from './animations';
 
 const Header: React.FC = () => {
   return (
-    <AnimatedContainer
-      variants={CONTAINER_ANIMATION}
-      initial="unMounted"
-      animate="mounted"
-    >
+    <AnimatedContainer>
       <Navbar />
     </AnimatedContainer>
   );
