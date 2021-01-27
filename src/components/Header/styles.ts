229fa92ec86cb40ab38035d58ac361d176fd648f
Lemuel Zara <lemuel.czara@gmail.com>
2021-01-27 00:43:51 -0300

    refactor: add position absolute to header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 
 export const AnimatedContainer = styled(motion.header)`
   width: 100%;
-  background: ${({ theme }) => theme.colors.primaryDark};
-  position: fixed;
   z-index: 100;
+  position: absolute;
 `;
