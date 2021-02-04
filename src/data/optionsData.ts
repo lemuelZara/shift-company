23c73b91d6626ed568c71d7a6bbd0e544a24a15a
Lemuel Zara <lemuel.czara@gmail.com>
2021-02-03 21:42:13 -0300

    refactor: move reasons info to a data object

---
+++ src/data/optionsData.ts
@@ -0,0 +1,77 @@
+import {
+  FaSlideshare,
+  FaHeadphonesAlt,
+  FaUsers,
+  FaCogs,
+  FaHandsHelping,
+  FaMixcloud,
+  FaAward,
+  FaDiagnoses,
+} from 'react-icons/fa';
+
+export const optionsData = [
+  {
+    id: '0',
+    shortTitle: 'Responsabilidade',
+    title: 'Responsabilidade',
+    description:
+      'Nossos sistemas processam 18% do volume de análises clínicas realizadas em todo o Brasil. Temos consciência de que estamos diretamente ligados à saúde de milhões de pessoas.',
+    icon: FaDiagnoses,
+  },
+  {
+    id: '1',
+    shortTitle: 'Tecnologia de ponta',
+    title: 'Tecnologia de ponta e plataforma 100% Web',
+    description:
+      'Nossa plataforma é 100% web, suportada pela tecnologia da InterSystems (Caché® e Ensemble®) que em conjunto com nossa expertise, proporciona conectividade, alta performance, escalabilidade, segurança e interoperabilidade entre sistemas.',
+    icon: FaMixcloud,
+  },
+  {
+    id: '2',
+    shortTitle: 'Capacitação e Conhecimento',
+    title: 'Capacitação e Conhecimento',
+    description:
+      'Equipes multidisciplinares, de desenvolvedores a biomédicos, se envolvem em todas as etapas da pré à pós implantação das nossas soluções, conferindo total autonomia aos usuários do sistema.',
+    icon: FaUsers,
+  },
+  {
+    id: '3',
+    shortTitle: 'Relacionamento e Parceria',
+    title: 'Relacionamento e Parceria',
+    description:
+      'Ao longo dos mais de 25 anos de história os desafios nos moveram e transformaram e assim continuaremos. Sabemos que a manutenção de relacionamentos próximos com nossos clientes e mercado é um dos fatores-chave de sucesso dessa trajetória.',
+    icon: FaHandsHelping,
+  },
+  {
+    id: '4',
+    shortTitle: 'Soluções completas',
+    title: 'Soluções completas e 100% integradas',
+    description:
+      'Proporcionamos um gerenciamento completo e total controle do laboratório desde a chegada do paciente à disponibilização dos resultados, trazendo para a operação maior produtividade, integração, economia de custos e inteligência de processos.',
+    icon: FaCogs,
+  },
+  {
+    id: '5',
+    shortTitle: 'Eficiência Operacional',
+    title: 'Eficiência Operacional e Segurança',
+    description:
+      'Atuamos com ampla visão da gestão laboratorial e nos preocupamos com a saúde e bem-estar das pessoas e também com a saúde empresarial de nossos clientes. Buscamos incessantemente contribuir para a produtividade das equipes e a qualidade dos serviços.',
+    icon: FaAward,
+  },
+  {
+    id: '6',
+    shortTitle: 'Suporte pleno',
+    title: 'Suporte pleno',
+    description:
+      'Conhecemos o negócio de nossos clientes e oferecemos suporte 24 horas e 7 dias por semana. Via portal de atendimento, telefone ou visitas técnicas, conte com nosso total apoio e comprometimento para alcançar seus resultados.',
+    icon: FaHeadphonesAlt,
+  },
+  {
+    id: '7',
+    shortTitle: 'Desenvolvimento Humano',
+    title: 'Desenvolvimento Humano',
+    description:
+      'Somos uma empresa de conhecimento e com uma equipe multidisciplinar com mais de 130 colaboradores. Nossa cultura organizacional é orientada para um ambiente de trabalho colaborativo e de inovação.',
+    icon: FaSlideshare,
+  },
+];
