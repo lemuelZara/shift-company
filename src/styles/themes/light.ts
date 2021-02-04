cdb980d4617ff4d151ceccd17b479b8753b3c7db
Lemuel Zara <lemuel.czara@gmail.com>
2021-02-03 21:47:14 -0300

    refactor: change light theme

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -22,6 +22,7 @@ export default {
   spacing: {
     default: '1.6rem',
     vertical: '1.6rem 0',
+    largeVertical: '2.4rem 0',
     horizontal: '0 1.6rem',
     large: '2.4rem',
     extraLarge: '4rem',
