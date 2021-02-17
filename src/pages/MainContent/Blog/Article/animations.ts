e73b7ee149fadda3f3524fc2d8e80789014ddf6e
Lemuel Zara <lemuel.czara@gmail.com>
2021-02-17 00:23:22 -0300

    refactor: remove animation

--- src/pages/MainContent/Blog/Article/animations.ts
+++ src/pages/MainContent/Blog/Article/animations.ts
@@ -7,13 +7,3 @@ export const IMAGE_ANIMATION = {
     },
   },
 };
-
-export const CONTAINER_ANIMATION = {
-  unMounted: {
-    opacity: 0,
-    transition: {
-      duration: 0.6,
-      ease: [0.43, 0.13, 0.23, 0.96],
-    },
-  },
-};
