cfae706015c99287830328c4870718911554fe89
Lemuel Zara <lemuel.czara@gmail.com>
2021-02-17 00:29:08 -0300

    refactor: create post data object

---
+++ src/data/postData.ts
@@ -0,0 +1,31 @@
+import ImagePostOne from '../assets/images/post_1.jpg';
+import ImagePostTwo from '../assets/images/post_2.jpg';
+import ImagePostThree from '../assets/images/post_3.jpg';
+
+export const postData = [
+  {
+    id: 1,
+    title:
+      'Saiba a importância de investir em capacitação profissional no laboratório',
+    image: ImagePostOne,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    publicationDate: '16 de fevereiro de 2021',
+  },
+  {
+    id: 2,
+    title: 'Conheça alguns indicadores de desempenho para o seu laboratório',
+    image: ImagePostTwo,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    publicationDate: '24 de fevereiro de 2021',
+  },
+  {
+    id: 3,
+    title: 'Conheça as vantagens da automação laboratorial',
+    image: ImagePostThree,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    publicationDate: '29 de janeiro de 2021',
+  },
+];
