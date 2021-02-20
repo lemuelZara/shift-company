ee00b6054abe81f4bc71ea757575b9b23a118653
Lemuel Zara <lemuel.czara@gmail.com>
2021-02-19 22:58:54 -0300

    refactor: change links menu

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,6 +1,6 @@
 export const menuData = [
-  { id: 1, title: 'Soluções', link: '/solucoes' },
-  { id: 2, title: 'Empresa', link: '/empresa' },
-  { id: 3, title: 'Carreiras', link: '/carreiras' },
-  { id: 4, title: 'Blog', link: '/blog' },
+  { id: 1, title: 'Soluções', link: '#' },
+  { id: 2, title: 'Empresa', link: '#' },
+  { id: 3, title: 'Carreiras', link: '#' },
+  { id: 4, title: 'Blog', link: '#' },
 ];
